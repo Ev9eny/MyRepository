ace9cdb76ba5485cc4cf8d25d07249f73afc0abb
Eugene <squeakyslippers@gmail.com>
2020-02-01 22:48:20 -0500

    variables

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String [] args) {
-        System.out.println("Hello world");
+        System.out.println("There once was a man named George,");
+        System.out.println("he was 70 years old. He really liked");
+        System.out.println("the name George, but didn't like being 70.");
+        System.out.println("Most of his friends were also 70, and one");
+        System.out.println("of them was also named George");
     }
 }
