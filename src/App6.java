262cc21dab86ec8e3b4f0e721753e5b5b0aaccde
Eugene <squeakyslippers@gmail.com>
2020-02-03 17:55:45 -0500

    game

---
+++ src/App6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App6 {
+    public static void main(String []args){
+        Scanner keyboardInput = new Scanner(System.in);
+        System.out.println("Enter a color: ");
+        String color = keyboardInput.nextLine();
+        System.out.println("Enter a plural noun: ");
+        String pluralNoun = keyboardInput.nextLine();
+        System.out.println("Enter a celebrity: ");
+        String celebrity = keyboardInput.nextLine();
+
+        System.out.println("Roses are " +color+ " \n"
+                +pluralNoun+ " are blue \n"
+                +celebrity+ " I love you");
+    }
+}
