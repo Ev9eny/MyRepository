d0bc273dc62bd04f323abdabab37c2036525c464
Eugene <squeakyslippers@gmail.com>
2020-02-05 18:55:15 -0500

    methods2

---
+++ src/App11.java
@@ -0,0 +1,11 @@
+public class App11 {
+    public static void main(String []args){
+
+        sayHi("Dave");
+        sayHi("Phyllys");
+        sayHi("Angela");
+    }
+    public static void sayHi(String name){
+        System.out.println("Hello " +name);
+    }
+}
