b74dbea023914dfbbfd92d6839844eaad1b25ac6
Eugene <squeakyslippers@gmail.com>
2020-02-05 17:48:58 -0500

    array2

---
+++ src/App8.java
@@ -0,0 +1,10 @@
+public class App8 {
+    public static void main(String []args){
+        String [] friends = new String[3];
+        friends[0]="Mike";
+        friends[1]="Karen";
+        friends[2]="Kevin";
+        System.out.println(friends[1]);
+
+    }
+}
