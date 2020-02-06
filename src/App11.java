794ed2b9b318ed21eb806f54438739d6936abe56
Eugene <squeakyslippers@gmail.com>
2020-02-06 10:20:15 -0500

    methods2

--- src/App11.java
+++ src/App11.java
@@ -1,11 +1,14 @@
 public class App11 {
     public static void main(String []args){
 
-        sayHi("Dave");
-        sayHi("Phyllys");
-        sayHi("Angela");
+        String myName = "Mike";
+        sayHi(myName, 78);
+        sayHi("Dave", 22);
+        sayHi("Phyllys", 44);
+        sayHi("Angela",25);
     }
-    public static void sayHi(String name){
+    public static void sayHi(String name, int age){
+
         System.out.println("Hello " +name);
     }
 }
