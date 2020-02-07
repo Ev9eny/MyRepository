bc1b3ca92b5f76401eec6c04c32f79d06a9436e2
Eugene <squeakyslippers@gmail.com>
2020-02-07 00:33:01 -0500

    return

---
+++ src/App12.java
@@ -0,0 +1,12 @@
+public class App12 {
+    public static void main(String []args){
+
+        int myNum = cube(3);
+        System.out.println(myNum);
+    }
+
+    public static int cube(int numberToCube){
+        System.out.println("Top");
+        return (numberToCube * numberToCube * numberToCube);
+    }
+}
---
+++ src/App13.java
@@ -0,0 +1,12 @@
+public class App13 {
+    public static void main(String []args){
+
+        String myNum = cube(3);
+        System.out.println(myNum);
+    }
+
+    public static String cube(int numberToCube){
+
+        return "Returned value";
+    }
+}
